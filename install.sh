d167d60b076a5f0311df41e8677801d459c6012b
ms.programmer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-03-01 08:58:26 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# This code write by (ms.nope)
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install python3
+sudo apt install python
+chmod +x hackingmenu.py
+echo ""
+echo "finish"
+echo ""
+echo "usage : ./hackingmenu.py"
