46615dde818109d7757348244c495f2b680fab0e
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-06-04 08:58:11 -0700

    Add files via upload

---
+++ hackingmenu.py
@@ -0,0 +1,159 @@
+# hackingmenu
+# This code write by (ms.nope)
+# version 1.0.0
+import os
+import time
+os.system("clear")
+class color:
+    green = '\033[92m'
+    red = '\033[91m'
+    blue = '\033[96m'
+    white = '\033[0m'
+# choose
+print(color.red + """
+      ( (   (   (  (      )    ) :            ) )  )    (   
+     )\)\: )\  (\ )\  ( ((.  ()))      ( (  (\(  ((.   )\  
+    (_)(_)(_() (\((_) )\))\ ()(())     )\)\  )(| ))\  ((_) 
+    | || |(_)()( ) |___)(_))/ _` |    (_((_) ()\((_))((_)) 
+    | __ | _` | _| / / | ' \)__. |    | '  \/ -_) ' \) || |
+    |_||_|__/_|__|_\_\_|_||_|___/     |_|_|_|___|_||_|\_._|
+    
+    (【H】【a】【c】【k】【i】【n】【g】   【m】【e】【n】【u】)
+""")
+print(color.red + "\t{1}."+color.blue+"testweb" , color.red + "\t \t {2}."+color.blue +"fsociety")
+print(color.red + "\t{3}."+color.blue+"brutex", color.red + "\t \t {4}."+color.blue +"phsihmailer")
+print(color.red + "\t{5}."+color.blue+"cupp",color.red + "\t \t {6}."+color.blue+"soial-Enginners-setoolkit")
+print(color.red + "\t{7}."+color.blue+"hackingtool" ,color.red + "\t {8}."+color.blue+"cam-hackers")
+print(color.red + "\t{9}."+color.blue+"Help" ,color.red + "\t \t {10}."+color.blue+"Exit")
+choose = str(input(color.green + "\nHacker/> " + color.white))
+# working
+if(str(choose) == '1'):
+    os.system("clear")
+    os.system("figlet testweb")
+    os.system("git clone https://github.com/msprogrammer2938/testweb")
+    os.system("clear")
+    try1 = str(input("press Enter... "))
+    if(str(try1) == ''):
+        os.system("python3 hackingmenu.py")
+    else:
+         os.system("python3 hackingmenu.py")
+elif(str(choose) == '2'):
+    os.system("clear")
+    os.system("figlet fsociety")
+    os.system("git clone https://github.com/Manisso/fsociety")
+    os.system("clear")
+    time.sleep(1)
+    print("finish !")
+    try2 = str(input("press Enter... "))
+    if(str(try2) == ''):
+        os.system("python3 hackermenu.py")
+elif(str(choose) == '3'):
+    os.system("clear")
+    os.system("figlet brutex")
+    os.system("git clone https://github.com/1N3/brutex")
+    os.system("clear")
+    time.sleep(2)
+    print("finish!")
+    try3 = str(input("press Enter... "))
+    if(str(try3) == ''):
+        os.system("python3 hackermenu.py")
+elif(str(choose) == '4'):
+    os.system("clear")
+    os.system("figlet phishmailer")
+    os.system("git clone https://github.com/BiZken/PhishMailer/ phishmailer/")
+    os.system("clear")
+    try4 = str(input("press Enter... "))
+    if(str(try4) == ''):
+        os.system("python3 hackermenu.py")
+elif(str(choose) == '5'):
+    os.system("clear")
+    os.system("figlet cupp")
+    os.system("git clone https://github.com/Mebus/cupp")
+    os.system("clear")
+    time.sleep(1)
+    print("finish")
+    try5 = str(input("press Enter... "))
+    if(str(try5) == ''):
+        os.system("python3 hackermenu.py")
+elif(str(choose) == '6'):
+    os.system("clear")
+    os.system("figlet set")
+    os.system("git clone https://github.com/trustedsec/social-engineer-toolkit/ set/")
+    os.system("clear")
+    time.sleep(1)
+    print("finish!")
+    try6 = str(input("press Enter... "))
+    if(str(try6) == ''):
+        os.sstem("python3 hackermenu.py")
+elif(str(choose) == '7'):
+    os.system("clear")
+    os.system("figlet hachingtool")
+    os.system("git clone https://github.com/Z4nzu/hackingtool")
+    os.system("clear")
+    time.sleep(1)
+    print("finish!")
+    try6 = str(input("press Enter... "))
+    if(str(try6) == ''):
+        os.system("python3 hackingmenu.py")
+    else:
+        os.system("python3 hackingmenu.py")
+elif(str(choose) == '8'):
+    os.system("clear")
+    os.system("figlet cam Hackers")
+    os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers/ camhackers/")
+    os.system("clear")
+    time.sleep(1)
+    print("finish!")
+    try7 = str(input("press Enter... "))
+    if(str(try7) == ''):
+        os.system("python3 hackermenu.py")
+elif(str(choose) == '10'):
+    os.system("clear")
+    print("good bye")
+    exit(1)
+elif(str(choose) == '9'):
+    os.system("clear")
+    print(color.green + """
+This code write by (ms.nope)
+usage:
+     1 - and install testweb
+     2 - and install fsociety
+     3 - and install brutex
+     4 - and install phishmailer
+     5 - and instal cupp
+     6 - aad install cupp))
+     7 - and install hackingtool
+     8 - and install camhackers
+     9 - and install informtions package !
+     10 - Exit package !
+""")
+    try9 = str(input(color.red + "press Enter... "))
+    if(str(try9) == ''):
+        os.system("python3 hackingmenu.py")
+    else:
+        os.system("python3 hackingmenu.py")
+elif(str(choose) == ''):
+    os.system("python3 hackermenu.py")
+elif(str(choose) == 'neofetch'):
+    os.system("clear")
+    print("""
+      ( (   (   (  (      )    ) :            ) )  )    (   
+     )\)\: )\  (\ )\  ( ((.  ()))      ( (  (\(  ((.   )\  
+    (_)(_)(_() (\((_) )\))\ ()(())     )\)\  )(| ))\  ((_) 
+    | || |(_)()( ) |___)(_))/ _` |    (_((_) ()\((_))((_)) 
+    | __ | _` | _| / / | ' \)__. |    | '  \/ -_) ' \) || |
+    |_||_|__/_|__|_\_\_|_||_|___/     |_|_|_|___|_||_|\_._|""")
+    try10 = str(input("\npress Enter... "))
+    if(str(try10) == ''):
+        os.system("python3 hackingmenu.py")
+    else:
+        os.system("python3 hackingmenu.py")
+else:
+    os.system("clear")
+    time.sleep(3)
+    print(color.red + "Error 532 !" + color.white)
+    time.sleep(2)
+    try8 = str(input(color.green + "press Enter... " + color.white))
+    if(str(try8) == ''):
+        os.system("python3 hackingmenu.py")
+# hackingmenu
