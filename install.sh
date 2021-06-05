24746167a78226df39acd0385984bbb6e296c21b
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-06-05 13:27:20 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 # This code write by (ms.nope)
+echo "Installing..."
+sleep 2
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install python3
 sudo apt install python
 chmod +x hackingmenu.py
+cd Update && chmod +x update
 echo ""
 echo "finish"
 echo ""
 echo "usage : ./hackingmenu.py"
+echo ""
